4fd7bded86f900fd2660123f002c1490387ab8eb
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-15 00:50:15 +0530

    Updated the 4th script with read concepts

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -3,5 +3,13 @@
 USERNAME=$1
 PASSWORD=$2
 
+echo "Please enter User Name::"
+
+read USERNAME
+
+echo "Please enter Password::"
+
+read PASSWORD
+
 echo "User Name is :: $USERNAME"
 echo "User Name is :: $PASSWORD"
