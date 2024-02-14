7486f93d17c14e12a1fef72b0a7d6a74c2c53c7f
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-15 00:19:55 +0530

    second script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, Good Morning"
+echo "Suresh:: Hi Ramesh, very good morning"
+echo "Ramesh:: How are you Suresh?"
+echo "Suresh:: I'm good Ramesh, how are you?"
