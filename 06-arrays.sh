aab8f9cb47b3c426632aac4623ff6150168956d0
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-15 01:16:25 +0530

    Updated the addition script again

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FRUITS=("Apple" "Banana Pomo")
+FRUITS=("Apple" "Banana" "Pomo")
 
 echo "1st value ${FRUITS[0]}"
 
-echo "1st value ${FRUITS[1]}"
+echo "2nd value ${FRUITS[1]}"
 
-echo "1st value ${FRUITS[2]}"
+echo "3rd value ${FRUITS[2]}"
