e7791de16270036b87aa0ae852138cde8f0db4b3
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-16 15:12:41 +0530

    Add new changes

---
+++ 10-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "ERROR:: Installing FAILED"
+        exit 1
+    else
+        echo "Installing is SUCCESS"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # You can give other than zero
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf install mysql -y
+
+VALIDATE
