46ed7144683523c79de5d16a7ccbcab7c999447e
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-16 19:33:50 +0530

    Updated the color codes

--- 11-logs.sh
+++ 11-logs.sh
@@ -9,19 +9,19 @@ NORMAL="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "ERROR:: Installing $2 $RED FAILED$NORMAL"
+        echo "ERROR:: Installing $2 $RED FAILED $NORMAL"
         exit 1
     else
-        echo "Installing $2 $GREEN SUCCESS$NORMAL"
+        echo "Installing $2 $GREEN SUCCESS $NORMAL"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "$RED ERROR$NROMAL:: Please run this script with root access" 
+    echo "$RED ERROR $NROMAL:: Please run this script with root access" 
     exit 1 # You can give other than zero
 else
-    echo "$GREEN You are root user$NORMAL"
+    echo "$GREEN You are root user $NORMAL"
 fi # fi means reverse of if, indicating condition end
 
 dnf install mysql -y &>> $LOGFILE
