1a5c4df0a714c389ab38b4b96c4bc7806396aae3
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-16 19:48:22 +0530

    Updated the color codes

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,6 +6,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+echo "Script execution started at:: $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
@@ -21,7 +22,7 @@ then
     echo -e "$R ERROR $N:: Please run this script with root access" 
     exit 1 # You can give other than zero
 else
-    echo -e "$G You are root user $N"
+    echo -e "$GYou are root user $N"
 fi # fi means reverse of if, indicating condition end
 
 dnf install mysql -y &>> $LOGFILE
