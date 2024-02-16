929a2d0eb7adfd4945796ed8dbd2324710b5a1a5
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-16 16:11:35 +0530

    Add new changes

--- 10-functions.sh
+++ 10-functions.sh
@@ -14,7 +14,7 @@ VALIDATE(){
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo "ERROR:: Please run this script with root access" 
     exit 1 # You can give other than zero
 else
     echo "You are root user"
@@ -22,8 +22,8 @@ fi # fi means reverse of if, indicating condition end
 
 dnf install mysql -y
 
-VALIDATE $? "MySQL"
+VALIDATE $? "MySQL...."
 
 dnf install git -y
 
-VALIDATE $? "GIT"
+VALIDATE $? "GIT...."
