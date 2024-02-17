8173c5eed6a997a605f54af338aac9938f74085e
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-17 21:22:11 +0530

    Add hello world simple programm

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -8,11 +8,35 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R ERROR:: Installation of $2 FAILED $N"
+        exit 1
+    else
+        echo -e " Installation of $2 $G SUCCESS $N"
+    fi
+}
+
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run as the root user $N"
+    exit 1
 else
     echo -e "$G You are root user $N"
 fi
 
-echo "All arguments passed:: $@"
+#echo "All arguments passed:: $@"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "$package....."
+    else
+        echo "Package aleready installed... $Y SKIPPING $N"
+    fi
+done
