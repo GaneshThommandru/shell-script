96dae86e5d713cefea4b53e277ad425ba8514f01
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-17 21:00:46 +0530

    Add hello world simple programm

---
+++ 13-install-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[33m"
+G="\e[32m"
+Y="\e[31m"
+N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run as the root user $N"
+else
+    echo -e "$G You are root user $N"
+fi
+
+echo "All arguments passed:: "
