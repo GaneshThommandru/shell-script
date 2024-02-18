53cc46763c3a8a92d3d63f9d49950da78b558560
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-18 09:46:57 +0530

    Added mongo repo file

--- Practice.sh
+++ Practice.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H:%M:%S)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+R="\e[31n"
+G="\e[32n"
+Y="\e[33n"
+N="\e[0n"
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-echo -e "Script Execution Started at :: $Y $TIMESTAMP $N"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$R ERROR:: $N Installing $2 $R FAILED $N"
+        echo -e "$2 .....$R FAILED $N"
         exit 1
     else
-        echo -e "Installing $2 $G SUCCESS $N"
-    fi 
+        echo -e "$2 .....$G SUCCESS $N"
+    fi
 }
-
-if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR:: $N Please run as root user"
-    exit 1
-else
-    echo -e "$G You are a root user $N"
-fi
-
-yum install mysql -y &>> $LOGFILE
-
-VALIDATE $? "MySQL...."
-
-yum install git -y &>> $LOGFILE
-
-VALIDATE $? "GIT...."
