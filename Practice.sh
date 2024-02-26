225cbf6780c420d0c865dfd082464bfdbc38225a
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-26 14:59:55 +0530

    updated practice script

--- Practice.sh
+++ Practice.sh
@@ -6,11 +6,15 @@ N="\e[0m"
 SOURCE_DIR=/tmp/shellscript-logs/
 test -d $SOURCE_DIR
 
+FILES_TO_DELETE=$(find /tmp/shellscript-logs -type f -mtime +14 -name "*.log")
+
 if [ $? -ne 0 ]
 then
     echo -e "$R ERROR::$N Source Directory not found"
     exit 1
 fi
 
-
-
+while IFS read -r line
+do 
+    echo "Deleting fiel : $line"
+done <<< $FILES_TO_DELETE
