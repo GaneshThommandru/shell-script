10111913d0259ceb976dc7333480caed2095ad80
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-26 14:04:40 +0530

    updated practice script

--- Practice.sh
+++ Practice.sh
@@ -6,12 +6,14 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-test -f /tmp/shellscript-logs
+SOURCE_DIR="/tmp/shellscript-logs"
+test -f $SOURCE_DIR
 
 if [ $? -ne 0 ]
 then
     echo -e "$R ERROR::$N Source Directory not found"
     exit 1
 fi
+
+
 
