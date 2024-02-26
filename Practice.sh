164ab767f68399e33116eb3412a9e9e6c71d9920
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-26 13:35:55 +0530

    updated practice script

--- Practice.sh
+++ Practice.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-ID=$(id -u)
+$SOURCE_DIR="/tmp/shellscript-logs"
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR="/tmp/shellscript-logs"
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-
-test -d $SOURCE_DIR
-
-if [ $? -ne 0 ]
-then
-    echo -e "$R Error:: Source directory doesn't exists $N"
-    exit 1
-fi
-
-while IFS= read -r line
-do
-    echo "$line"
-    rm -rf $line
-done <<< $FILES_TO_DELETE
+echo -e "$R Check the applied color $N"
