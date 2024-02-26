843d55c744d4474c88b5edfc0f4a3f43a968a991
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-02-26 16:12:23 +0530

    updated practice script

--- Practice.sh
+++ Practice.sh
@@ -1,6 +1,15 @@
-#/bin/bash
+#!/bin/bash
 
-while IFS= read -r line
-do 
-    echo "$line"
-done <<< $(find /tmp/shellscript-logs -type f)
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File' | awk '{print $6F}' | cut -d % -f1)
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do
+    if [ $line -ge $DISK_THRESHOLD ]
+    then
+        message+="Alert:: Disk Usage of $(df -hT | grep -vE 'tmp|File' | awk '{print $1F}') : $DISK_USAGE\n"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message:\n$message"
