eeddc440a69c32fd8ea1ad2d547ef0ce9d5a4095
Ganesh Thommandru <ganeshkumar.tommandru@gmail.com>
2024-03-01 00:26:47 +0530

    Update

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 NAME=""
-WISHES=""
+WISHES="Good Morning"
 
 USAGE(){
     echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
     echo "-n, specifies the name(mandatory)"
-    echo "-w, specifies the wishes. ex, Good Morning"
+    echo "-w, specifies the wishes. (Optional).  ex, Default is Good Morning"
     echo "-h, Displays Help and Exit" 
 }
 
@@ -21,8 +21,10 @@ while getopts ":n:w:h" opt; do
     esac
 done
 
-if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-    echo "ERROR:: Both -n and -w are madatory options"
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then #Both <name> and <wishes> are mandatory
+if [ -z "$NAME" ]; then # now wishes is optional
+    #echo "ERROR:: Both -n and -w are mandatory options."
+    echo "ERROR:: -n madatory option"
     USAGE;
     exit 1
 fi
